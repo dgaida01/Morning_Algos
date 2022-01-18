d651d71d8725a083e5ce1831f0ff36150882fc0a
dgaida01 <dean.gaida@hotmail.com>
2022-01-18 11:11:02 -0500

    convert string to word array

---
+++ stringToWordArray.js
@@ -0,0 +1,28 @@
+
+let strToWordArr =(str)=>{
+ let arr = [];
+ let start = 0;
+
+
+
+    while(str[start]==" "){        
+        start++;       
+    }
+ 
+ for(let i=start; i<str.length; i++){    
+    
+    if(str[start]==' ' && str[i]==' '){
+        start++;
+        continue;
+    }
+    
+    if(str[i]==" " || i==str.length-1){   
+        arr=arr.concat(str.substring(start,i));
+        start=i+1;   
+ }
+}
+return arr
+}
+
+let myword="happy"
+console.log(strToWordArr(`   ${myword}    welcome to the Jungle baby!        `));
